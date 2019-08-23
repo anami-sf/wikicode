4f36531d22baacef5a19fc9b6c62a1efa4119c51
anami823 <anami823@gmail.com>
2019-08-22 21:57:26 -0700

    Comment mongoose config in config/database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
-mongoose.connect(process.env.DATABASE_URL,
-    {useNewUrlParser: true}
-)
+// mongoose.connect(process.env.DATABASE_URL,
+//     {useNewUrlParser: true}
+// )
 
 
-//Message to verify connection
-var db = mongoose.connection;
+// //Message to verify connection
+// var db = mongoose.connection;
 
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
-})
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
+// })
