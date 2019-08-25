5bee7168dc44d66b76cc0649e63376bfeada0e8d
anami823 <anami823@gmail.com>
2019-08-24 21:15:15 -0700

    Create step schema

--- models/recipe.js
+++ models/recipe.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose')
 
+var stepSchema = new mongoose.Schema({
+    stepDescription: String,
+    code: String
+});
+
 var recipeSchema = new mongoose.Schema({
     category: String,
     stepNo: Number,
     stepDescription: String, 
-    code: String, 
+    steps: [stepSchema], 
     //votes: [{type: Schema.Types.OjectId, ref: 'Vote'}],
     //gists: [{type: Schema.Types.OjectId, ref: 'Gist'}],
-    server: String,
+    webServer: String,
     database: String,
     appFramework: String,
     host: String
