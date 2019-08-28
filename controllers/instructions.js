e044cafa0cad29002fb96d08a5279dc9dfa7dee7
anami823 <anami823@gmail.com>
2019-08-27 23:13:25 -0700

    Implement Serah's (designer) styling and fix referencing bug

--- controllers/instructions.js
+++ controllers/instructions.js
@@ -3,15 +3,17 @@ const Instruction = require('../models/instruction')
 
 const create = (req, res) => {
     Recipe.findById(req.params.id)
-    //req.body.recipe = recipe._id
     .then((recipe)=> {
+        req.body.recipe = recipe._id
         console.log('instructionCtl.create: ', req.body)
         Instruction.create(req.body)
         .then(instruction => {
-            recipe.instructions.push(instruction)
-            recipe.save()
+            recipe.instructions.push(instruction._id)
+            recipe.save(function(err) {
+                if (err) return handleError(err)
+                res.redirect(`/recipes/${recipe._id}`)
+            })
         })
-        res.redirect(`/recipes/${recipe._id}`)
     })
     .catch((err)=> {
         console.log(err)
